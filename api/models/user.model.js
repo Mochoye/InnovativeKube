d2282aede4d1f750e1fabc97ed086a19951b41be
Archisman Ray <95351969+Mochoye@users.noreply.github.com>
2024-01-22 00:24:04 +0530

    api commits

---
+++ api/models/user.model.js
@@ -0,0 +1,30 @@
+import mongoose from 'mongoose';
+
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    profilePicture: {
+      type: String,
+      default:
+        'https://static.vecteezy.com/system/resources/previews/009/734/564/non_2x/default-avatar-profile-icon-of-social-media-user-vector.jpg',
+    },
+  },
+  { timestamps: true }
+);
+
+const User = mongoose.model('User', userSchema);
+
+export default User;
---
+++ api/routes/predict.route.js
@@ -0,0 +1,8 @@
+import express from 'express';
+import {calories } from '../controllers/predict.controller.js';
+
+const router = express.Router();
+
+router.post('/calories', calories);
+
+export default router;
